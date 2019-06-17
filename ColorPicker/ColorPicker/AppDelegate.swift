502c92060577f397942431f095101e2f10fab412
vanessashe <48176062+vanessashe@users.noreply.github.com>
2019-06-17 14:13:47 +0800

    fix bug

--- ColorPicker/ColorPicker/AppDelegate.swift
+++ ColorPicker/ColorPicker/AppDelegate.swift
@@ -7,9 +7,9 @@
 //
 
 import UIKit
-let screenWidth = UIScreen.main.bounds.width
-let screenHeight = UIScreen.main.bounds.height
-
+//let screenWidth = UIScreen.main.bounds.width
+//let screenHeight = UIScreen.main.bounds.height
+//
 @UIApplicationMain
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
